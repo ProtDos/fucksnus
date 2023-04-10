caa5dc55baafda8fb1746951b212df74730d528d
ProtDos <69071809+ProtDos@users.noreply.github.com>
2023-04-10 16:34:06 +0200

    Update fucksnus.py

--- fucksnus.py
+++ fucksnus.py
@@ -1,42 +1,22 @@
 import requests
+import sys
 import json
-import argparse
 
 class FuckSnus:
-    def __init__(self, api_key):
-        self.api_key = api_key
+    def __init__(self):
+        pass
 
-    def search(self, terms):
-        search_data = {
-            "terms": terms,
-            "types": ["username", "email", "lastip", "hash", "password", "name"],
-            "auth_code": self.api_key
-        }
-        with requests.Session() as session:
-            search_response = session.post("https://one.snusbase.com/data/search", json=search_data)
-            combo_results = {}
-            for term in terms:
-                combo_url = f"https://beta.snusbase.com/v2/combo/{term}"
-                combo_response = session.get(combo_url)
-                combo_results[term] = combo_response.json()
-        search_result = search_response.json()
-        combined_result = {"search_result": search_result, "combo_results": combo_results}
-        with open('snusbase_results.json', 'w') as f:
-            json.dump(combined_result, f, indent=6)
-        return combined_result
+    def search(self, term):
+        f = requests.get(f"https://beta.snusbase.com/v2/combo/{term}")
+        out = f.json()["result"]
+        l = []
+        for item in out:
+            for item2 in out[item]:
+                l.append(item2)
+        with open(f"output_{term}.json", "w") as file:
+            json.dump(l, file)
 
 
 if __name__ == '__main__':
-    api_key = "Dkfjdsakljfdsfajkkljsdaj"
-    search = FuckSnus(api_key)
-
-    parser = argparse.ArgumentParser(description='Search Snusbase for given terms.')
-    parser.add_argument('num_terms', type=int, help='Number of search terms to enter.')
-    args = parser.parse_args()
-
-    terms = []
-    for i in range(args.num_terms):
-        term = input(f"Enter search term {i+1}: ")
-        terms.append(term)
-
-    result = search.search(terms)
+    search = FuckSnus()
+    search.search(sys.argv[1])
